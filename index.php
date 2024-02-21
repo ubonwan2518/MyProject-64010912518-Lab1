89f1e55ed18f1a6efa872ff1c48025dbcf30b8fd
ubonwan2518 <155517139+ubonwan2518@users.noreply.github.com>
2024-02-21 14:16:51 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en" dir="ltr">
+<?php
+include("../check.php");
+?>
+<head>
+    <meta charset="utf-8">
+    <title>Admin</title>
+    <link rel="stylesheet" href="../css/admin.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <script>
+
+    </script>
+
+</head>
+
+<body>
+
+    <div class="sidebar">
+        <header>Admin</header>
+        <ul>
+
+            <li><a href="store_logout.php"><i class="fas fa-stream"></i>Store</a></li>
+            <li><a href="order.php"><i class="fas fa-stream"></i>Order</a></li>
+            <li><a href="stock.php"><i class="fas fa-stream"></i>Stock</a></li>
+            <li><a href="category.php"><i class="fas fa-stream"></i>Category</a></li>
+            <li><a href="logout.php"><i class="fas fa-stream"></i>Logout</a></li>
+        </ul>
+    </div>
+
+
+    <div class="order ">
+
+    <div class="ms-5 position-absolute top-50 start-50 translate-middle">
+        <h1 class="opacity-50 ms-5" >โปรดเลือกเมนูด้านซ้าย</h1>
+    </div>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
